8de7d58462f0baa0652c81d1b7ff7c44a26be25b
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-01-05 12:35:07 -0500

    Add files via upload

---
+++ window.js
@@ -0,0 +1,53 @@
+// window.alert("Hello")
+// //alert creates a popup
+
+// window.prompt('What is your favorite sport')
+
+// if (confirm('Are you sure?')){
+//     console.log('Yes')
+
+// }else{
+//     console.log('No')
+// }
+
+
+//outter height and width
+let val;
+val=window.outerHeight
+
+console.log(val)
+
+val=window.outerWidth
+console.log(val)
+
+//inner height and width
+val=window.innerHeight
+
+console.log(val)
+
+val=window.innerWidth
+console.log(val)
+
+//location object
+val=window.location;
+console.log(val);
+val=window.location.hostname;
+console.log(val);
+val=window.location.port;
+console.log(val);
+val=window.location.href;
+console.log(val);
+val=window.location.search;
+console.log(val);
+
+
+//redirect
+// window.location.href= 'http://google.com'
+
+
+//reload
+// window.location.reload()
+
+//history object
+// window.history.go()
+
